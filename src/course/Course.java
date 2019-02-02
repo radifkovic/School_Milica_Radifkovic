a5a48252d09b75547e26c0b903caf4e203ee0487
Milica Radifković <milica.radifkovic@gmail.com>
2019-02-02 19:50:58 +0100

    makes Course class

---
+++ src/course/Course.java
@@ -0,0 +1,45 @@
+package course;
+
+public class Course {
+
+    String name;
+    int numberOfClasses;
+    String codeName;
+
+    public Course(){
+        this.name = "QA";
+        this.numberOfClasses = 34;
+        this.codeName = "September 2018.";
+    }
+    
+    public Course(String name, int numberOfClasses, String codeName) {
+        this.name = name;
+        this.numberOfClasses = numberOfClasses;
+        this.codeName = codeName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfClasses() {
+        return numberOfClasses;
+    }
+
+    public void setNumberOfClasses(int numberOfClasses) {
+        this.numberOfClasses = numberOfClasses;
+    }
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+
+}
--- src/student/Student.java
+++ src/student/Student.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package student;
 
-/**
- *
- * @author Milica
- */
 public class Student {
-    
+
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+
+    public Student() {
+        this.firstName = "Milica";
+        this.lastName = "Radifković";
+        this.yearOfBirth = 1988;
+    }
+
+    public Student(String firstName, String lastName, int yearOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public void info() {
+        System.out.println("First name: " + this.firstName);
+        System.out.println("Last name: " + this.lastName);
+        System.out.println("Year of birth: " + this.yearOfBirth);
+
+    }
 }
